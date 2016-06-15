60dce379ce61dc7e37d1915ffd63b19513766589
matthewwoo <matthew.edan.woo@gmail.com>
2016-06-14 22:44:05 -0700

    Added ability to get post based on time

--- src/app.py
+++ src/app.py
@@ -2,41 +2,25 @@ from flask import Flask, request
 
 from src.errors import Error
 from src.models.bots.bot import Bot
+import src.common.constants as Constants
 
 app = Flask(__name__)
-app.secret_key = "123"
-# app.config.from_object('config')
 
-# from src.models.bots.views import bot_blueprint
-# app.register_blueprint(bot_blueprint, url_prefix="/bot")
 @app.route("/bot_call", methods=['POST'])
 def get_channel_info():
     if request.method == 'POST':
         try:
+            text = request.form['text']
             channel = request.form['channel_id']
             user_id = request.form['user_id']
             user = request.form['user_name']
         except:
             raise Error.ParameterError("Parameters are not correct")
         try:
-            Bot(channel_id=channel, reply_user_id=user_id, reply_user_name=user).post_message()
+            Bot(channel_id=channel, reply_user_id=user_id, reply_user_name=user).query_wit(text=text)
         except:
             raise Error.MessageError("Message is incorrect")
-        return (get_messages(channel), 200)
-
-def get_messages(channel):
-    Bot().get_last_messages(channel)
-
+        return 200
 
 if __name__ == '__main__':
     app.run(port=4999, debug=True)
-
-
-        # token=4wZgtfCemPr0FCHdl9DKgObD&team_id=T1042HMJB
-# team_domain=929broderick&\
-# service_id=34895149248&\
-# channel_id=C103W0BRC&channel_name=general&\
-# timestamp=1460758091.000022&\
-# user_id=U103W08NA&\
-# user_name=matthew\
-# text=instapaper&trigger_word=instapaper
--- src/models/bots/bot.py
+++ src/models/bots/bot.py
@@ -1,8 +1,11 @@
+import calendar
 import json
 import re
 import time
+import urllib2
+import uuid
 
-import datetime as datetime
+from datetime import datetime
 import time
 
 import requests
@@ -14,46 +17,71 @@ from src.models.articles.article import Article
 class Bot(object):
     def __init__(self, bot_token=BotConstants.TOKEN,
                  bot_username=BotConstants.BOTNAME,
+                 session_id=None,
                  channel_id=None,
                  reply_user_id=None,
                  reply_user_name=None):
-
         self.bot_token = bot_token
         self.username = bot_username
+        self.session_id = uuid.uuid4() if session_id is None else session_id
         self.channel_id = channel_id if channel_id is not None else BotConstants.CHANNEL
         self.reply_user_id = reply_user_id if reply_user_id is not None else BotConstants.REPLY_USER_ID
         self.reply_user_name = reply_user_name if reply_user_name is not None else BotConstants.REPLY_USER_NAME
 
-    #2016-04-16_update
-    # def __repr__(self):
-    #     print "{}, {}".format(self.token, self.username)
 
-    def post_message(self):
+    def query_wit(self, text):
+        text = text.encode('utf-8')
+        print text
+        encode_text = urllib2.quote(text)
+        print encode_text
+        data_url = 'https://api.wit.ai/converse?v=20160330&session_id={}&q={}'.format(self.session_id, encode_text)
+        data = requests.post(data_url, headers=BotConstants.WIT_HEADERS)
+        json_data = json.loads(data.text)
+        print json_data
+        if 'datetime' in json_data['entities']:
+            start_time = json_data['entities']['datetime'][0]['value']
+            start_time = calendar.timegm(datetime.strptime(start_time[:23], '%Y-%m-%dT%H:%M:%S.%f').timetuple())
+            self.get_messages(channel=self.channel_id, start_time=start_time)
+        else:
+            self.fail_message()
+        return 200
+
+
+    def success_message(self, num_articles):
         sc = SlackClient(self.bot_token)
         sc.api_call(
-            "chat.postMessage", channel=self.channel_id, text="Hello <@{}|{}> grabbing your instapaper links :newspaper:!".format(self.reply_user_id, self.reply_user_name),
-            username=self.username, as_user="false", icon_emoji=':robot_face:'
+            "chat.postMessage", channel=self.channel_id,
+            text="Hello <@{}|{}> just grabbed {} new articles for you :newspaper:!".format(self.reply_user_id, self.reply_user_name, num_articles),
+            username=self.username, as_user="false", icon_emoji=':instapaper:'
         )
+        return 200
 
-    def get_messages(self, channel, start_time=None):
-        sc = SlackClient(self.bot_token)
-        start_time = time.time()- 90061 if start_time is None else start_time
-        json_data = sc.api_call("channels.history", channel=channel, oldest = start_time)
-        message_dump =json.dumps(json_data)
-        message_dict = json.loads(message_dump)
-        for test in message_dict['messages']:
-            print 'title_link' in test
 
-    def get_last_messages(self, channel):
+    def fail_message(self):
         sc = SlackClient(self.bot_token)
-        json_data = sc.api_call("channels.history", channel=channel, count=10)
+        sc.api_call(
+            "chat.postMessage", channel=self.channel_id,
+            text="Sorry <@{}|{}> not sure if I can do that".format(self.reply_user_id, self.reply_user_name),
+            username=self.username, as_user="false", icon_emoji=':instapaper:')
+        return 200
+
+    def get_messages(self, channel=None, start_time=None):
+        channel = self.channel_id if channel is None else channel
+        sc = SlackClient(self.bot_token)
+        if start_time == None:
+            json_data = sc.api_call("channels.history", channel=channel, count=10)
+        else:
+            json_data = sc.api_call("channels.history", channel=channel, oldest=start_time)
         message_dump = json.dumps(json_data)
         message_dict = json.loads(message_dump)
-        for test in message_dict['messages']:
-            if 'attachments' in test:
-                x = test['attachments'][0]['from_url']
+        num_articles = 0
+        for msg in message_dict['messages']:
+            if 'attachments' in msg:
+                x = msg['attachments'][0]['from_url']
                 Article().save_to_instapaper(url=x)
                 print x
+                num_articles += 1
+        return self.success_message(num_articles=num_articles)
 
 
     def get_links(self):
