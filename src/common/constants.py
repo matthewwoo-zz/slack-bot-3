bfc9e91d2bee8d51db40c0c500cd0644ec18aa41
matthewwoo <matthew.edan.woo@gmail.com>
2016-06-15 08:46:14 -0700

    Removed constants

--- src/common/constants.py
+++ src/common/constants.py
@@ -1,25 +1,11 @@
 
 import os
 
-# TOKEN = os.environ.get('TOKEN')
-# USERNAME = os.environ.get('USERNAME')
-# CHANNEL = os.environ.get('CHANNEL')
-# BOTNAME = os.environ.get('BOTNAME')
-# REPLY_USER_ID = os.environ.get('REPLY_USER_ID')
-# REPLY_USER_NAME = os.environ.get('REPLY_USER_NAME')
-# WIT_HEADERS = {'Authorization': '{}'.format(os.environ.get('WIT_TOKEN'))}
-
-TOKEN = "xoxb-34239333142-KXVW87uh4XorYp8BtPyc8vLf"
-USERNAME = "@U10719T46"
-CHANNEL = "C103W0BRC"
-USERNAME2 = "<@U10719T46>"
-BOTNAME = "instapaper"
-INSTAUSERNAME = "matthewedanwoo"
-INSTAPASSWORD = "Ivey3583"
-INSTAEMAIL = "matthew.edan.woo@gmail.com"
-INSTAURL ="https://medium.com/observations-thoughts-opinions/the-age-of-boundless-of-variety-through-abstraction-1917df4106f0#.2n47n9g6o"
-REPLY_USER_ID = "U103W08NA"
-REPLY_USER_NAME = "matthew"
-WIT_HEADERS = {'Authorization': 'Bearer ZMBL5O5DLYANHE2XFIEZHTFTBZAMILSP'}
-
+TOKEN = os.environ.get('TOKEN')
+USERNAME = os.environ.get('USERNAME')
+CHANNEL = os.environ.get('CHANNEL')
+BOTNAME = os.environ.get('BOTNAME')
+REPLY_USER_ID = os.environ.get('REPLY_USER_ID')
+REPLY_USER_NAME = os.environ.get('REPLY_USER_NAME')
+WIT_HEADERS = {'Authorization': '{}'.format(os.environ.get('WIT_TOKEN'))}
 
